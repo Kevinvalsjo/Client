e16315a0886a7bdf2271e4b5fb74d52795cb5c5f
kevin.valsjo <kevin.valsjo@elev.ga.ntig.se>
2024-05-17 13:40:01 +0200

    funkar här också

--- src/Main.java
+++ src/Main.java
@@ -25,42 +25,49 @@ public class Main {
         String msg,msg2,msg3,msg4,msg0="";
 
         chatMulticastSocket.joinGroup(group);
+boolean hej=true;
+while(hej) {
+    System.out.println("Vill du 1.Addera 2.Multiplicera 3.Avsluta");
+
+    msg0 = br.readLine();
+    DatagramPacket data5 = new DatagramPacket(msg0.getBytes(), 0, msg0.length(), group, portnumber);
+    chatMulticastSocket.send(data5);
+    int case0 = Integer.parseInt(msg0);
+    switch (case0) {
+        case 1 -> {
+            System.out.println("Skriv en siffra");
+            msg = br.readLine();
+            DatagramPacket data = new DatagramPacket(msg.getBytes(), 0, msg.length(), group, portnumber);
+            chatMulticastSocket.send(data);
+
+            System.out.println("Skriv en siffra");
+            msg2 = br.readLine();
+            DatagramPacket data2 = new DatagramPacket(msg2.getBytes(), 0, msg2.length(), group, portnumber);
+            chatMulticastSocket.send(data2);
+        }
+        case 2 -> {
+            System.out.println("Skriv en siffra");
+            msg3 = br.readLine();
+
+            DatagramPacket data3 = new DatagramPacket(msg3.getBytes(), 0, msg3.length(), group, portnumber);
+            chatMulticastSocket.send(data3);
+
+            System.out.println("Skriv en siffra");
+            msg4 = br.readLine();
+            DatagramPacket data4 = new DatagramPacket(msg4.getBytes(), 0, msg4.length(), group, portnumber);
+            chatMulticastSocket.send(data4);
+
+        }
+        case 3 -> {
+hej=false;
+            chatMulticastSocket.close();
+
+        }
+
+    }
 
 
-        System.out.println("Vill du 1.Addera 2.Multiplicera");
-
-        msg0 = br.readLine();
-        DatagramPacket data5 = new DatagramPacket(msg0.getBytes(), 0, msg0.length(), group, portnumber);
-        chatMulticastSocket.send(data5);
-        int case0 =Integer.parseInt(msg0);
-                switch (case0) {
-                    case 1 -> {
-                        System.out.println("Skriv en siffra");
-                        msg = br.readLine();
-                        DatagramPacket data = new DatagramPacket(msg.getBytes(), 0, msg.length(), group, portnumber);
-                        chatMulticastSocket.send(data);
-
-                        System.out.println("Skriv en siffra");
-                        msg2 = br.readLine();
-                        DatagramPacket data2 = new DatagramPacket(msg2.getBytes(), 0, msg2.length(), group, portnumber);
-                        chatMulticastSocket.send(data2);
-                    }
-                    case 2 ->{
-                        System.out.println("Skriv en siffra");
-                        msg3 = br.readLine();
-
-                        DatagramPacket data3 = new DatagramPacket(msg3.getBytes(), 0, msg3.length(), group, portnumber);
-                        chatMulticastSocket.send(data3);
-
-                        System.out.println("Skriv en siffra");
-                        msg4 = br.readLine();
-                        DatagramPacket data4 = new DatagramPacket(msg4.getBytes(), 0, msg4.length(), group, portnumber);
-                        chatMulticastSocket.send(data4);
-
-                    }
-
-                }
-                chatMulticastSocket.close();
+}
 
     }
 }
